18552047a553827e8e245f0bc0f6e62b2f86f87f
athensai <122765545+athensai@users.noreply.github.com>
2023-04-05 13:12:37 -0400

    Update Research.py

--- pages/Research.py
+++ pages/Research.py
@@ -68,3 +68,55 @@ st.sidebar.title("Settings")
 
 handle = st.text_input("Enter a Twitter handle:")
 n = st.sidebar.slider("Number of tweets to analyze:", 100, 500, 1000)
+
+if st.button("Fetch Data"):
+    try:
+        st.write(f"Fetching {n} tweets for @{handle}")
+        handles = [handle]
+        df = fetch_twitter_data(handles, n)
+        st.write("Data fetched successfully.")
+        st.write(df.head())
+
+        st.write("Generating embeddings...")
+        embeddings_matrix = generate_embeddings(df)
+        st.write("Embeddings generated.")
+
+        st.write("Clustering the data...")
+        scaler = StandardScaler()
+        scaled_embeddings = scaler.fit_transform(embeddings_matrix)
+        optimal_k = 3  # Replace this with the optimal k value you found from the plot
+        kmeans = KMeans(n_clusters=optimal_k, random_state=42)
+        clusters = kmeans.fit_predict(scaled_embeddings)
+
+        pca = PCA(n_components=2)
+        embeddings_2d = pca.fit_transform(scaled_embeddings)
+
+        st.write("Visualizing the clusters...")
+        plot_clusters(embeddings_2d, clusters)
+
+        num_examples = 30  # Adjust this value based on your needs
+
+        for cluster_idx in range(optimal_k):
+            cluster_data = np.array(scaled_embeddings)[clusters == cluster_idx]
+            distances = [np.linalg.norm(embedding - kmeans.cluster_centers_[cluster_idx]) for embedding in cluster_data]
+            closest_examples_idx = np.argsort(distances)[:num_examples]
+
+            st.write(f"\nCluster {cluster_idx}:")
+            for idx in closest_examples_idx:
+                st.write(f"- {df.loc[idx, 'Author']} + {df.loc[idx, 'Text']}")
+
+        # Computing similarity scores
+        st.write("Computing similarity scores...")
+        phrase = st.text_input("Enter a phrase to compute similarity scores:")
+        if phrase:
+            phrase_embedding = get_embedding(phrase)
+            df['Similarity'] = df.apply(lambda row: cosine_similarity(phrase_embedding, row['embedding']), axis=1)
+            st.write(df.head())
+
+            st.write("Visualizing similarity over time...")
+            plot_similarity(df)
+
+    except Exception as e:
+        st.write("An error occurred while fetching or processing data.")
+        st.write(e)
+
